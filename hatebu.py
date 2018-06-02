96efad95adb152f29a7eb6eb24760a38131cbdcd
AKIYAMA <jinbeezame@gmail.com>
2018-06-02 21:52:53 +0900

    Update hatebu.py

--- hatebu.py
+++ hatebu.py
@@ -1,15 +1,15 @@
-  1 # はてなブックマークのコメントを標準出力に出力する
-  2 import urllib.request
-  3 import urllib.parse
-  4 import sys
-  5 import json
-  6
-  7 url = sys.argv[1]
-  8 encurl = urllib.parse.quote(url, safe='')
-  9 resp = urllib.request.urlopen('http://b.hatena.ne.jp/entry/jsonlite/' + encurl)
- 10 b64str = resp.read()
- 11 bkmstr = b64str.decode()
- 12 bkminfo = json.loads(bkmstr)
- 13 for it in bkminfo['bookmarks'] :
- 14     if it['comment'] != '':
- 15         print(it['comment'])
+# はてなブックマークのコメントを標準出力に出力する
+import urllib.request
+import urllib.parse
+import sys
+import json
+
+url = sys.argv[1]
+encurl = urllib.parse.quote(url, safe='')
+resp = urllib.request.urlopen('http://b.hatena.ne.jp/entry/jsonlite/' + encurl)
+b64str = resp.read()
+bkmstr = b64str.decode()
+bkminfo = json.loads(bkmstr)
+for it in bkminfo['bookmarks'] :
+    if it['comment'] != '':
+         print(it['comment'])
