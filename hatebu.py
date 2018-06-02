413b5162b610c97f611e5b0f44930565c3b019db
Ubuntu <katsumi@katsumi.wpac4x1hui3uthvkjjsep3ph0h.bx.internal.cloudapp.net>
2018-06-02 13:01:39 +0000

    はてブがついていないページを指定すると例外が発生したので抑止する

--- hatebu.py
+++ hatebu.py
@@ -10,6 +10,8 @@ resp = urllib.request.urlopen('http://b.hatena.ne.jp/entry/jsonlite/' + encurl)
 b64str = resp.read()
 bkmstr = b64str.decode()
 bkminfo = json.loads(bkmstr)
+if bkminfo is None:
+    sys.exit()
 for it in bkminfo['bookmarks'] :
     if it['comment'] != '':
          print(it['comment'])
